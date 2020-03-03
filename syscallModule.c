6a71b0a97602541987a68900cea31d04b11f50ee
BMatulonis <60149447+BMatulonis@users.noreply.github.com>
2020-03-03 18:00:56 -0500

    Basic elevator module uploaded
    Finished the basic structure for the elevator module, still need to implement linked list, kmalloc, etc.

---
+++ syscallModule.c
@@ -0,0 +1,175 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/linkage.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+#define BUF_LEN 2000
+#define OFFLINE 0
+#define IDLE 1
+#define LOADING 2
+#define UP 3
+#define DOWN 4
+
+/*
+typedef struct Lists {
+	struct list_head queue[10];	// list of passengers on each floor
+	struct list_head elev;		// list of passengers on elevator
+} Lists;
+*/
+static struct proc_dir_entry* proc_entry;
+char* msg
+char* msg2
+static int procfs_buf_len;
+static char passFloor[10][BUF_LEN]; // char representation of passengers waiting on each floor
+static int waitFloor[10];	// num of passengers waiting at each floor
+int state;
+int animals;	// 0: none, 1: dog, 2: cat
+int currentFloor;
+int passengers;
+int weight;	// max load is 15
+int waiting;	// total number waiting
+int serviced;	// total number serviced
+int stopping;
+static char str[10];
+char onFloor;
+
+extern long (*STUB_start_elev)(void);
+long start_elev(void) {
+	printk(KERN_NOTICE "Start elevator Test\n");
+	if (state == OFFLINE)
+	{
+		state = IDLE;
+		currentFloor = 1;
+		passengers = 0;
+		weight = 0;
+		waiting = 0;
+		animals = 0;
+		serviced = 0;
+		return 0;
+	}
+	else	// elevator already active
+		return 1;
+}
+
+extern long (*STUB_issue_elev)(int,int,int,int);
+long issue_elev(int num_pets, int pet_type, int start_floor, int destination_floor) {
+	printk(KERN_NOTICE "Issue Request Test\n");
+	return 0;
+}
+
+extern long (*STUB_close_elev)(void);
+long close_elev(void) {
+	printk(KERN_NOTICE "Stop elevator Test\n");
+	// offload all current passengers
+	if (stopping == 1)
+		return 1;
+	stopping = 1;
+	state = OFFLINE;
+	return 0;
+}
+
+static int procfile_open(struct inode* inode, struct file* file) {
+	printk(KERN_INFO "proc_open\n");
+	msg = kmalloc(sizeof(char) * 2048, __GFP_RECLAIM);
+	msg2 = kmalloc(sizeof(char) * 2048, __GFP_RECLAIM);
+
+	// change direction to string
+	if (state == OFFLINE)
+		sprintf(str, "OFFLINE");
+	else if (state == IDLE)
+		sprintf(str, "IDLE");
+	else if (state == LOADING)
+		sprintf(str, "LOADING");
+	else if (state == UP)
+		sprintf(str, "UP");
+	else if (state == DOWN)
+		sprintf(str, "DOWN");
+	else
+		sprintf(str, "ERROR");
+	sprintf(msg, "Elevator state: %s\n", str);
+	if (animals == 1)
+		sprintf(str, "dog");
+	else if (animals == 2)
+		sprintf(str, "cat");
+	else
+		sprintf(str, "none");
+	sprintf(msg2, "Elevator animals: %s\n", str);
+	strcat(msg, msg2);
+	sprintf(msg2, "Current floor: %d\n", currentFloor);
+	strcat(msg, msg2);
+	sprintf(msg2, "Number of passengers: %d\n", passengers);
+	strcat(msg, msg2);
+	sprintf(msg2, "Current weight: %d\n", weight);
+	strcat(msg, msg2);
+	sprintf(msg2, "Number of passengers waiting: %d\n", waiting);
+	strcat(msg, msg2);
+	sprintf(msg2, "Number of passengers serviced: %d\n\n\n", serviced);
+	strcat(msg, msg2);
+	// print queue interface
+	int i = 10;
+	while (i > 0) {
+		if (i == currentFloor)
+			onFloor = '*';
+		else
+			onFloor = ' ';
+		sprintf(msg2, "[%c] Floor %*d: %*d   ", onFloor, 2, i, 4, waitFloor[i]);
+		strcat(msg2, passFloor[(char)i]);
+		strcat(msg2, "\n");
+		strcat(msg, msg2);
+		i--;
+	}
+}
+
+static ssize_t procfile_read(struct file* file, char* ubuf, size_t count, loff_t* ppos) {
+	printk(KERN_INFO "proc_read\n");
+	procfs_buf_len = strlen(msg);
+	if (*ppos > 0 || count < procfs_buf_len)
+		return 0;
+	if (copy_to_user(ubuf, msg, procfs_buf_len))
+		return -EFAULT;
+	*ppos = procfs_buf_len;
+	printk(KERN_INFO "gave to user %s\n", msg);
+	return procfs_buf_len;
+}
+
+static struct file_operations procfile_fops = {
+	.owner = THIS_MODULE,
+	.read = procfile_read,
+	.open = procfile_open,
+};
+
+static int elevator_init(void) {
+	proc_entry = proc_create("elevator", 0666, NULL, &procfile_fops);
+	if (proc_entry == NULL)
+		return -ENOMEM;
+	STUB_start_elev = &(start_elev);
+	STUB_issue_elev = &(issue_elev);
+	STUB_close_elev = &(close_elev);
+/*
+	int i = 0;
+	while (i < 10) {
+		INIT_LIST_HEAD(&queue[i]);
+		i++;
+	}
+	INIT_LIST_HEAD(&elev);
+*/
+	return 0;
+}
+
+static void elevator_exit(void) {
+	proc_remove(proc_entry);
+	STUB_start_elev = NULL;
+	STUB_issue_elev = NULL;
+	STUB_close_elev = NULL;
+	return;
+}
+
+module_init(elevator_init);
+module_exit(elevator_exit);
+
